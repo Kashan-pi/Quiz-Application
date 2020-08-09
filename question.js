17c46e84e302aa37ae036e3f223bf5a7a3d72851
Kashan-pi <m.hassanshafiq@hotmail.com>
2020-08-09 18:51:35 +0500

    Ready to commit

---
+++ question.js
@@ -0,0 +1,41 @@
+var questions = [{
+        "question": "What is the result of 2+2? ",
+        "option1" : "3",
+        "option2" : "2",
+        "option3" : "4",
+        "option4" : "1",
+        "answer" : "option3"
+    },{
+        "question": "What is the result of 5+5? ",
+        "option1" : "10",
+        "option2" : "15",
+        "option3" : "5",
+        "option4" : "50",
+        "answer"  : "option1" 
+    },{
+
+      "question": "What is the result of 10+10? ",
+        "option1" : "3",
+        "option2" : "2",
+        "option3" : "20",
+        "option4" : "1",
+        "answer" : "option3"
+    },{
+
+        "question": "What is the result of 1+1? ",
+        "option1" : "2",
+        "option2" : "0",
+        "option3" : "10",
+        "option4" : "1",
+        "answer" : "option1"
+    },{
+
+        "question": "What is the result of 25+25? ",
+        "option1" : "20",
+        "option2" : "29",
+        "option3" : "70",
+        "option4" : "50",
+        "answer" : "option4"
+    },
+      
+]
